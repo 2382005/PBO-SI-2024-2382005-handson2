11f415fa7a3473b475bad973caf66505e0d2a41d
Gabriel Blessing Christian Hutagaol <2381002@unai.edu>
2024-09-10 10:12:56 +0700

    Handson 2

---
+++ BlockVSExpressionVsStatement.java
@@ -0,0 +1,10 @@
+public class BlockVSExpressionVsStatement {
+    public static void main(String[] args) {
+        int a = 10 + 10;
+        System.out.println("Rachel Ultraman");
+        System.out.println("Rachel Ultraman");
+        System.out.println("Rachel Ultraman");
+        System.out.println("Rachel Ultraman");
+
+    }
+}
